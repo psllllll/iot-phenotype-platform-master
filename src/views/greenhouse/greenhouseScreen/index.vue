b182642373b4c663d39edf344a306ffcdbf44da0
Sayakuree <salytsukasa014@gmail.com>
2023-07-25 14:35:25 +0800

    页面初始化

---
+++ src/views/greenhouse/greenhouseScreen/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="greenhouse-screen">
+      页面大屏
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/greenhouse/passDataScreen/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="pass-data-screen-container">
+      过去的数据们
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
